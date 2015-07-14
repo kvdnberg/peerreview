ec2a9f531fb835e59544c150b568b1ab25da0a0c
Karin van den Berg <karin@netwerven.nl>
2015-07-14 11:18:12 +0200

    Replace blade by twig

--- resources/views/PeerReview/Developers/add.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('app')
-
-@section('content')
-    <h1>{{trans('Add a developer')}}</h1>
-
-    {!! Form::model(new App\Developer, ['route' => 'developers']) !!}
-
-    @include('PeerReview.Developers.form')
-@stop
--- resources/views/PeerReview/Developers/edit.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('app')
-
-@section('content')
-    <h1>{{trans('messages.edit_developer')}}</h1>
-    {!! Form::model($developer, ['route' => ['developers_update', $developer->id]]) !!}
-
-
-    @include('PeerReview.Developers.form')
-@stop
