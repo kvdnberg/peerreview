1ec0ba38a4586a0d7b396030edc6203d1b84fc61
Karin van den Berg <karin@netwerven.nl>
2015-07-14 16:58:07 +0200

    Routes fix and renames

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,11 +13,11 @@
 
 Route::get('/', 'PeerReviewController@index');
 Route::get('edit/{id?}', ['middleware' => 'auth.basic', 'uses' => 'PeerReviewController@edit']);
-Route::get('developers/add', ['middleware' => 'auth.basic', 'as' => 'developers_add', 'uses' => 'DevelopersController@create']);
-Route::get('developers/edit/{id}', ['middleware' => 'auth.basic', 'as' => 'developers_edit', 'uses' => 'DevelopersController@edit']);
-Route::post('developers/update/{id}', ['middleware' => 'auth.basic', 'as' => 'developers_update', 'uses' => 'DevelopersController@update']);
+Route::get('developers/add', ['middleware' => 'auth.basic', 'as' => 'addDeveloper', 'uses' => 'DevelopersController@create']);
+Route::get('developers/edit/{id}', ['middleware' => 'auth.basic', 'as' => 'editDeveloper', 'uses' => 'DevelopersController@edit']);
+Route::post('developers/update/{id}', ['middleware' => 'auth.basic', 'as' => 'updateDeveloper', 'uses' => 'DevelopersController@update']);
 Route::post('developers', ['middleware' => 'auth.basic', 'uses' =>'DevelopersController@store']);
 
-Route::get('developers/{sortby?}/{order?}', ['middleware' => 'auth.basic', 'as' => 'developers', 'uses' =>'DevelopersController@index']);
+Route::get('developers/{sortby?}/{order?}', ['middleware' => 'auth.basic', 'as' => 'showDevelopers', 'uses' =>'DevelopersController@index']);
 
 Route::post('saveReviewBoard', ['middleware' => 'auth.basic','uses' => 'PeerReviewController@store']);
