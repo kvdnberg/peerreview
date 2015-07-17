6c18aaeaa9130d9b47f4053aa58b517514889414
Karin van den Berg <karin@netwerven.nl>
2015-07-17 10:21:55 +0200

    Javascript documentation

--- resources/assets/js/peerreviewboard.js
+++ resources/assets/js/peerreviewboard.js
@@ -1,4 +1,7 @@
-
+/**
+ * On page load, read local storage and load the board from there.
+ * @param board - ID of the board and local storage key (i.e. frontend_peerReviewBoard)
+ */
 function restoreLocalData(board)
 {
     var localData = localStorage.getItem(board);
@@ -13,16 +16,32 @@ function restoreLocalData(board)
     }
 }
 
+/**
+ * HTML5 draggable
+ * @param ev
+ */
 function allowDrop(ev)
 {
     ev.preventDefault();
 }
 
+/**
+ * HTML5 draggable
+ * @param ev
+ */
 function drag(ev)
 {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
+/**
+ * HTML5 draggable
+ *
+ * Checks whether or not a drop is allowed (i.e. is it the right column? not dropping in the wrong element?)
+ *
+ * @param ev
+ * @param allowMultiple - whether or not the target element can contain more than one child
+ */
 function drop(ev, allowMultiple)
 {
     ev.preventDefault();
@@ -49,7 +68,7 @@ function dropNotAllowed(ev, allowMultiple, data, board)
         return true;
     }
     if($('#' + ev.target.id).hasClass('reviewBoardEntry')) {
-        return true; //no developers inside developers! 
+        return true; //no developers inside developers!
     }
     var column = findColumn(ev.target.id);
     if(column) {
@@ -59,6 +78,10 @@ function dropNotAllowed(ev, allowMultiple, data, board)
 
 }
 
+/**
+ * Clears the local data key for the given board
+ * @param board: ID of the board and localstorage key (i.e. frontend_peerReviewBoard)
+ */
 function resetLocalData(board)
 {
     if(board) {
@@ -66,6 +89,11 @@ function resetLocalData(board)
     }
 }
 
+/**
+ * Checks whether the target is an author or reviewer column (used in dropNotAllowed to check if the column matches the dropped item)
+ * @param targetId - ID of the target element
+ * @returns {string} - author/reviewer
+ */
 function findColumn(targetId)
 {
     var columns = ['author', 'reviewer'];
@@ -77,6 +105,11 @@ function findColumn(targetId)
     }
 }
 
+/**
+ * After dropping an element in or out of the board, read the current state and store this in localstorage.
+ *
+ * @param board - ID of the board and local storage key (i.e. frontend_peerReviewBoard)
+ */
 function updateLocalData(board)
 {
     if(board) {
@@ -92,6 +125,11 @@ function updateLocalData(board)
     }
 }
 
+/**
+ * Reads the current state from localstorage and returns a stringified JSON array to send to the DB
+ * @param type - frontend/backend
+ * @returns {*} - stringified JSON array i.e. jsonArray['frontend'][0]['author'] = 7 (devId)
+ */
 function getReviewBoardJSON(type)
 {
             var jsonArray = {};
@@ -117,7 +155,11 @@ function getReviewBoardJSON(type)
 
 }
 
-function storePeerReviewBoard(type, _token)
+/**
+ * Send the current state of the given board type to store in the DB
+ * @param type - frontend/backend
+ */
+function storePeerReviewBoard(type)
 {
     var boardResult = getReviewBoardJSON(type);
     $.ajax({
