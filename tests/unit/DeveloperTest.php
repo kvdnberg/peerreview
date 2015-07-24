cca0f255b1dbe87cbcf47b9600d76220bc85039c
Karin van den Berg <karin@netwerven.nl>
2015-07-24 11:24:30 +0200

    Codeception

---
+++ tests/unit/DeveloperTest.php
@@ -0,0 +1,39 @@
+<?php
+
+class DeveloperTest extends TestCase {
+
+    /**
+     * @param array $nameParts
+     * @param string $expectedResult
+     *
+     * @dataProvider providerTestDeveloperFullName
+     */
+	public function testDeveloperFullName(array $nameParts, $expectedResult)
+	{
+		$developer = new App\Models\Developer;
+        $developer->firstName = $nameParts['firstName'];
+        $developer->middleName = $nameParts['middleName'];
+        $developer->lastName = $nameParts['lastName'];
+
+        $result = $developer->getFullNameAttribute();
+
+		$this->assertEquals($expectedResult, $result);
+	}
+
+    public function providerTestDeveloperFullName()
+    {
+        return [
+            [
+                ['firstName' => 'Karin', 'middleName' => 'van den', 'lastName' => 'Berg'],
+                'Karin van den Berg'
+            ],
+            [
+                ['firstName' => 'Jakub', 'middleName' => '', 'lastName' => 'Gadkowski'],
+                'Jakub Gadkowski'
+            ],
+        ];
+    }
+
+
+
+}
